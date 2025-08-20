6e42fa4088318e3a70c4454e87f2bc2ab28f8049
Ikejiri-1 <pedroike21@gmail.com>
2025-08-20 17:49:44 -0300

    styled components instalado e criação do botão

---
+++ src/components/AbButton/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export function AbButton() {
+  return <button>Clique aqui!</button>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
-
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   /** custom content, defaults to 'the snozzberries taste like snozzberries' */
   children?: ReactChild;
@@ -13,3 +12,5 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
 export const Thing: FC<Props> = ({ children }) => {
   return <div>{children || `the snozzberries taste like snozzberries`}</div>;
 };
+
+export * from './components/AbButton';
